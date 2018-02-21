bf4b025c3b2364af7e1f4364cdc399afc13d3152
ManetteS <Manette@172.17.3.70>
2018-02-21 15:13:35 +0100

    Completed mongoDB import file

--- mongodb/import.js
+++ mongodb/import.js
@@ -5,6 +5,36 @@ var fs = require('fs');
 var MongoClient = mongodb.MongoClient;
 var mongoUrl = 'mongodb://localhost:27017/911-calls';
 
+var nullifyEmptyValues = function(str) {
+  return (str !== undefined && str.trim() !== "") ? str.trim() : null;
+}
+
+var createCall = function(data) {
+  const lat = nullifyEmptyValues(data["lat"]);
+  const lng = nullifyEmptyValues(data["lng"]);
+  var location = null;
+  if (lat && lng) {
+    location = [parseFloat(lng), parseFloat(lat)];
+  }
+
+  var category = null;
+  var description = null;
+  if (data["title"] !== undefined) {
+    category = data["title"].split(":")[0].trim();
+    description = data["title"].split(":")[1].trim();
+  }
+
+  return {
+	"location": location,
+	"zip": parseFloat(data["zip"]),
+	"category" : category,
+	"description" : description,
+	"@timestamp": new Date(data["timeStamp"]),
+	"quarter": nullifyEmptyValues(data["twp"]),
+	"address": nullifyEmptyValues(data["addr"])
+    };
+}
+
 var insertCalls = function(db, callback) {
     var collection = db.collection('calls');
 
@@ -12,7 +42,7 @@ var insertCalls = function(db, callback) {
     fs.createReadStream('../911.csv')
         .pipe(csv())
         .on('data', data => {
-            var call = {}; // TODO créer l'objet call à partir de la ligne
+            var call = createCall(data);
             calls.push(call);
         })
         .on('end', () => {
